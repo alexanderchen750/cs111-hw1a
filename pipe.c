de12274e1f2d0af836331e8f2c6d51476bc0666f
alexanderchen750 <alexander.chen750@gmail.com>
2024-04-26 15:53:26 -0700

    second

--- pipe.c
+++ pipe.c
@@ -5,73 +5,116 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-int main(int argc,char *argv[])
+int normalRun(char *exe, int readPrevPipe)
 {
-	if(argc == 1)
-	{
+	int fds[2];
+	if(pipe(fds)<0){
+		//errorExit("pipe fail");
+		perror("pipe fail");
 		exit(1);
 	}
-
-	int fds[(argc-1)*2];
-	pid_t pids[argc-1];
-	
-	
-	for(int i = 0; i < argc - 1; i++)
+	pid_t processID = fork();
+	if(processID < 0){
+		//fork failed
+		fprintf(stderr, "fork\n");
+		exit(1);
+	}
+	else if(processID ==0)
 	{
-		if(i < argc-2) // Pipe if not last command
-		{
-			pipe(fds + i*2);
-			printf("Pipe for %d and %d read fd = %d, write fd = %d\n", i+i%2, i%2+i+1, fds[i+i%2], fds[i%2+1+i]);
-		}
-		pids[i] = fork();
-		
-		if(pids[i]<0) //fork failed
-		{
-			
-			fprintf(stderr, "fork\n");
-			exit(1);
-		}
-		else if(pids[i] == 0)
-		{
-			/*for(int k = 0; k < (i+1)*2; k+=2){
-				printf("Pipe for %d and %d read fd = %d, write fd = %d\n", k, k+1, fds[k], fds[k+1]);
-			}
-			printf("\n");*/
-			if(i < argc-2) // Change Write Command if not last
-			{
-				dup2(fds[(2*i)+1],1);
-			}
-			if(i > 0) //if not first command, connect to read of prev pipe
-			{
-				dup2(fds[(2*i)-2],0);
-			}
-
-			for(int j = 0; j < 2*(argc - 2); j++) //close fds
-			{
-				close(fds[j]);
-			}
-			execlp(argv[i+1],argv[i+1],NULL); //run command
-			perror("execlp");
-			exit(1);
-		}
-		if(i > 0)
-		{
-			close(fds[(2*i)-2]);
-		}
-		if(i < argc-2) // Change Write Command if not last
-		{
-			close(fds[2*i+1]);
-		}
+		dup2(readPrevPipe,0);
+		dup2(fds[1],1);
+		close(fds[0]);
+		close(fds[1]);
+		execlp(exe,exe,NULL);
+		perror("execlp");
+        exit(1);
 		
 	}
+	else{
+		close(readPrevPipe);
+		close(fds[1]);
+		waitpid(processID, NULL, 0);
+		return fds[0];
+	}
+	return -1;
+}
 
-	for(int i = 0; i < (argc-2)*2; i++)
-	{
-		close(fds[i]);
+int firstRun(char *exe)
+{
+	int fds[2];
+	if(pipe(fds)<0){
+		//errorExit("pipe fail");
+		perror("pipe fail");
+		exit(1);
 	}
-	for(int i = 0; i < argc-1; i++)
-	{
-		waitpid(pids[i], NULL, 0);
+	pid_t processID = fork();
+	if(processID < 0){
+		//fork failed
+		fprintf(stderr, "fork\n");
+		exit(1);
 	}
-	return 0;
+	else if(processID ==0)
+	{
+		dup2(fds[1],1);
+		close(fds[0]);
+		close(fds[1]);
+		execlp(exe,exe,NULL);
+		perror("execlp");
+        exit(1);
+		
+	}
+	else{
+		close(fds[1]);
+		waitpid(processID, NULL, 0);
+		return fds[0];
+	}
+	return -1;
+}
+
+void lastRun(char *exe, int readPrevPipe)
+{
+	int fds[2];
+	if(pipe(fds)<0){
+		//errorExit("pipe fail");
+		perror("pipe fail");
+		exit(1);
+	}
+	pid_t processID = fork();
+	if(processID < 0){
+		//fork failed
+		fprintf(stderr, "fork\n");
+		exit(1);
+	}
+	else if(processID ==0)
+	{
+		dup2(readPrevPipe,0);
+		close(fds[0]);
+		close(fds[1]);
+		execlp(exe,exe,NULL);
+		perror("execlp");
+        exit(1);
+		
+	}
+	else{
+		close(readPrevPipe);
+		close(fds[1]);
+		waitpid(processID, NULL, 0);
+	}
+
+}
+int main(int argc,char *argv[])
+{
+	if(argc < 2)
+	{
+		exit(EINVAL);
+	}
+	if(argc == 2)
+	{
+		execlp(argv[1],argv[1], NULL);
+	}
+	int readPrevPipe = firstRun(argv[1]);
+	for(int i =2; i <argc-1; i++){
+		readPrevPipe = normalRun(argv[2], readPrevPipe);
+	}
+	lastRun(argv[argc-1],readPrevPipe);
 }
